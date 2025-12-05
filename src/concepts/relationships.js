77e0bfc34f059686397f0f3a69e756c30bd0858d
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-12-05 21:49:59 +0600

    refactor: Updated concepts/relationships.js

--- src/concepts/relationships.js
+++ src/concepts/relationships.js
@@ -20,6 +20,23 @@ async function createPostsTable() {
 }
 //w: (end)  ╰──────────── createPostsTable ────────────╯
 
+//w: (start)╭──────────── insertNewPost ────────────╮
+async function insertNewPost(title, content, userId) {
+  const insertPostQuery = ` 
+    INSERT INTO posts (title, content, user_id)
+    VALUES ($1, $2, $3)
+    RETURNING *
+`;
+  try {
+    const result = await db.query(insertPostQuery, [title, content, userId]);
+    return result.rows;
+  } catch (error) {
+    console.log(error, "relationships.js", 32);
+  }
+}
+//w: (end)  ╰──────────── insertNewPost ────────────╯
+
 module.exports = {
   createPostsTable,
+  insertNewPost,
 };
